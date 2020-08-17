08f0bb96c15a7829d6d60765ed34ae914cc3f5bf
Arkroshex <igorbraga.rsantos@gmail.com>
2020-08-17 15:43:06 -0300

    Exercicio de vetor

---
+++ vetores.js
@@ -0,0 +1,38 @@
+/*Vetores e objetos
+
+Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
+
+Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
+
+As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo:
+
+{ nome: 'C++', especialidade: 'Desktop' }
+{ nome: 'Python', especialidade: 'Data Science' }
+{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
+
+Por exemplo:
+
+const objeto = {
+  propriedade: [
+    { nome: "C++", especialidade: "Desktop" },
+    { nome: "JavaScript", especialidade: "Web/Mobile" }
+  ]
+};
+
+Imprima em tela o nome e especialidade da primeira tecnologia que o usuário utiliza no seguinte formato:
+
+O usuário Carlos tem 32 anos e usa a tecnologia C++ com especialidade em Desktop
+
+*/
+
+const programador = {
+    nome: "Carlos",
+    idade: 32,
+    tecnologia: [
+        { nome: "C++", especialidade: "Desktop" },
+        { nome: "JavaScript", especialidade: "Web/Mobile" }
+    ]
+
+}
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologia[0].nome} com especialidade em ${programador.tecnologia[0].especialidade}`)
