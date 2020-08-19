5af223a839b45e9e6d0eaa2da031f56a00d34a14
Arkroshex <igorbraga.rsantos@gmail.com>
2020-08-18 21:08:36 -0300

    Added some features to my avg calculator"

--- index.js
+++ index.js
@@ -1,58 +1,54 @@
-//Criar um programa que calcule média de notas
-// recebendo duas notas em variáveis separadas
-
- 
 const alunosDaTurma1 = [
     {
         nome: 'João',
-        nota: 7
+        nota: 10,
     },
     {
         nome: 'Carlos',
-        nota: 10
+        nota: 10,
     },
     {
         nome: 'Mateus',
-        nota: 5
-    }
+        nota: 3,
+
+
+    },
 ]
 const alunosDaTurma2 = [
     {
         nome: 'Mariana',
-        nota: 0
+        nota: 0,
     },
     {
         nome: 'Julia',
-        nota: 4
+        nota: 0,
     },
     {
         nome: 'Catarina',
-        nota: 8
+        nota: 0,
     },
     {
         nome: 'Fulano',
-        nota: 10
+        nota: 10,
     },
     {
         nome: 'Roberta',
-        nota: 10
-    }
+        nota: 10,
+    },
 ]
- 
-function calculoMedia(alunos){
+
+function calculoMedia(alunos) {
     let soma = 0
-   for (let index = 0; index < alunos.length; index++) {
-       soma = soma + alunos[index].nota;
-   }
-   const media = soma / alunos.length;
-   return (media);
-    
+    for (let index = 0; index < alunos.length; index++) {
+        soma = soma + alunos[index].nota;
+    }
+    const media = soma / alunos.length;
+    return (media);
+
 }
 
-const media1 = calculoMedia(alunosDaTurma1);
-const media2 = calculoMedia(alunosDaTurma2);
-
-function mensagem(media){
+function mensagemDaMedia(turma) {
+    let media = calculoMedia(turma);
     if (media >= 5) {
         console.log(`Parabéns, pessoal! A média da turma é ${media}`);
     } else {
@@ -60,5 +56,36 @@ function mensagem(media){
     }
 }
 
-mensagem(media1);
-mensagem(media2);
+function reprovar(aluno) {
+
+    aluno.reprovado = (aluno.nota < 5);
+}
+
+
+function mostrarReprovacao(aluno) {
+
+    if (aluno.reprovado) {
+
+        console.log(`O ${aluno.nome} está reprovado!`);
+    } else {
+        console.log(`O ${aluno.nome} está aprovado!`);
+    }
+}
+
+function enviaMensagem(alunos) {
+
+    for (let aluno of alunos) {
+
+        reprovar(aluno);
+        mostrarReprovacao(aluno);
+    }
+}
+
+
+mensagemDaMedia(alunosDaTurma1);
+
+mensagemDaMedia(alunosDaTurma2);
+
+enviaMensagem(alunosDaTurma1);
+
+enviaMensagem(alunosDaTurma2);
